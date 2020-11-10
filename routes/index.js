1d80ac3b6f4ec8a5c1b5bbf7a8aae52c3e847470
61010637 <pondparinyaaa@gmail.com>
2020-11-10 16:38:26 +0700

    route url

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,12 @@ router.get('/', function(req, res, next) {
     res.render('index', { title: 'Express' });
 });
 
-router.get('/register', (req, res, next) => {
-    res.render('register');
+router.get('/login', (req, res, next) => {
+    res.render('login');
 });
+
+router.get('/cart', (req, res, next) => {
+    res.render('cart');
+})
 
 module.exports = router;
