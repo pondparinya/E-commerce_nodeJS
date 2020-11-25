9c32d5eda9e82a89564928d8d6e3ce564ff6e053
61010637 <pondparinyaaa@gmail.com>
2020-11-25 19:32:31 +0700

    95%

--- config/auth.js
+++ config/auth.js
@@ -2,7 +2,7 @@ exports.isAdmin = function(req, res, next) {
     if (req.isAuthenticated() && req.user.admin == 1) {
         next();
     } else {
-        res.redirect('/login');
+        res.redirect('/');
     }
 };
 
@@ -10,6 +10,6 @@ exports.isUser = function(req, res, next) {
     if (req.isAuthenticated()) {
         next();
     } else {
-        res.redirect('/login')
+        res.redirect('/')
     }
 }
